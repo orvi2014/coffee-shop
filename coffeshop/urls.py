bb8ffd30e210b9c437cf9725415ac07c71e34780
orvi2014 <das.orvi@yahoo.com>
2018-08-31 00:41:04 +0600

    login endpoint partial done

--- coffeshop/urls.py
+++ coffeshop/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
+
+api_urls = [
+    #url(r'^todos/', include('todos.urls', namespace='todos')),
+    url(r'^users/', include('users.urls', namespace='users')),
+]
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api_urls)),
 ]
