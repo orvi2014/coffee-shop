eca8259e50d4a246a1ccaf47b36cb63a209a51f1
orvi2014 <das.orvi@yahoo.com>
2018-09-04 16:45:56 +0600

    delte work left

--- order/views.py
+++ order/views.py
@@ -29,14 +29,29 @@ class OrderDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
     permission_classes = (UserIsOwnerOrder, )
-    end = str(datetime.now())
-    end_ed = datetime.strptime(end, '%Y-%m-%d %H:%M:%S.%f')
-    start_st = datetime.strptime(OrderListCreateAPIView.start, '%Y-%m-%d %H:%M:%S.%f')
-    timedelta= end_ed - start_st
-    minutes=timedelta.seconds % 3600 / 60.0
-    minutes=int(round(minutes))
+    minutes_str="0"
+    minutes=0
+    # Get method
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+    # update method
     def put(self, request, *args, **kwargs):
-        if self.minutes<1:
+        # custom default_error_messages
+        default_error_messages = {
+            'error_messages': _('Update or delete within 15 minutes')
+        }
+        # Can not update after 15 minutes_str
+        # Don't reload
+        # It's open a new session everytime. Just wait here 15 mins it would
+        # url need to change in future
+        end = str(datetime.now())
+        end_ed = datetime.strptime(end, '%Y-%m-%d %H:%M:%S.%f')
+        start_st = datetime.strptime(OrderListCreateAPIView.start, '%Y-%m-%d %H:%M:%S.%f')
+        timedelta= end_ed - start_st
+        minutes_str=timedelta.seconds % 3600 / 60.0
+        minutes=int(round(minutes_str))
+
+        if minutes<0:
             return self.update(request, *args, **kwargs)
+        #else:
+        #    raise serializers.ValidationError(self.error_messages['error_messages'])
