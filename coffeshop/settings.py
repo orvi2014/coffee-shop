9fb29b42da0b01e4c20f2341c22ab44bda219240
orvi2014 <das.orvi@yahoo.com>
2018-09-04 11:37:23 +0600

    1st version deploy

--- coffeshop/settings.py
+++ coffeshop/settings.py
@@ -121,6 +121,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
